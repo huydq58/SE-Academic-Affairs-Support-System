2e1efd0023614e7b9ebb52f4b8580988e7737e4f
Quang Huy <22520539@gm.uit.edu.vn>
2026-02-28 16:23:35 +0700

    Add project files.

---
+++ SE Academic Affairs Support System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SE_Academic_Affairs_Support_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
